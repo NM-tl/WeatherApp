b9de6597cdbf0a588dccecf312577ae08aec58b1
NM <nikitam42015@gmail.com>
2023-01-31 20:20:16 +0200

    commite geo

--- src/data/Data.js
+++ src/data/Data.js
@@ -4,24 +4,24 @@ export const DataContext = createContext();
 
 export const DataContextProvider = ({children}) => {
 
-    const [geo, setGeo] = useState({});
+    // const [geo, setGeo] = useState({});
     const [data, setData] = useState({});
 
 const API_KEY = 'baccda4f1d5019e8d22bd6c22787afb8';
 
     useEffect(() => {
 
-      const getGeo = async () => {
+    //   const getGeo = async () => {
 
-        navigator.geolocation.getCurrentPosition((success) => {
-           setGeo(success.coords);
+    //     navigator.geolocation.getCurrentPosition((success) => {
+    //        setGeo(success.coords);
 
-           console.log(success)
+    //        console.log(success)
             
-      })
-    };
+    //   })
+    // };
     
-      getGeo();
+      // getGeo();
 
       const getWeather = async () => {
       
